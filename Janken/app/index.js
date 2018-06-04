bacf7316fe4cb48fe779f40dad2568d7b160221e
山本憲 <em072029@gmail.com>
2018-06-05 00:05:59 +0900

    package.json更新し、ざっくりフォルダ構成を作成

---
+++ Janken/app/index.js
@@ -0,0 +1,12 @@
+import { stackNavigator } from 'react-navigation'
+import Home from './screens/home'
+
+config RouteConfigs = {
+  Home: {
+    screen: Home,
+  },
+}
+
+const App = stackNavigator(Routeconfigs);
+
+export default App = stackNavigator(Routeconfigs);
---
+++ Janken/screens/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {View, Alert } from 'react-native'
+import {NavigationActions} from 'react-navigation'
+import HomeComponent from './../components/homeComponent'
+
+export default Home extends Component {
+  static navigationOptions = {
+    title: 'Home',
+  }
+  constructor(){
+    super()
+  }
+  render(){
+    return (
+      <View>
+      
+      </View>
+    )
+  }
+}
