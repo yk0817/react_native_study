c25eaec4293c6a3c8bad72b30edd70d724ba740b
山本憲 <em072029@gmail.com>
2018-06-13 23:35:04 +0900

    Add react-navigation

--- github-client/App.js
+++ github-client/App.js
@@ -1,58 +1,13 @@
-import React, { Component } from 'react';
-import { 
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  FlatList
-} from 'react-native';
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import Home from './Home';
 
-export default class App extends React.Component<{}> {
-  state = {
-    items: [],
-    refreshing: false,
-  }
-  page = 0;
 
-  fetchRepositories(refreshing = false){
-    const newPage = refreshing ? 1 : this.page + 1;
-    this.setState({ refreshing });
-    fetch(`https://api.github.com/search/repositories?q=react&page=${newPage}`)
-      .then(response => response.json())
-      .then(({ items }) => {
-        this.page = newPage;
-        if (refreshing) {
-          this.setState({ items });
-        } else {
-          this.setState({ items: [...this.state.items, ...items] });
-        }
-    });        
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity style={{marginTop: 20}} onPress={() => this.fetchRepositories()}>
-          <Text>Fetch</Text>
-        </TouchableOpacity>
-        <FlatList
-          data={this.state.items}
-          renderItem={({ item }) => <Text style={{padding: 20}}>{item.name}</Text>}
-          keyExtractor={(item) => item.id}
-          onEndReached={() => this.fetchRepositories(true)}
-          onEndReachedThreshold={0.1}
-          onRefresh={() =>{}}
-          refreshing={this.state.refreshing}
-          />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
+export default StackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: 'Home',
+    },
   },
 });
-/[^\d]/
---
+++ github-client/Home.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { 
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
+
+export default class App extends React.Component<{}> {
+  state = {
+    items: [],
+    refreshing: false,
+  }
+  page = 0;
+
+  fetchRepositories(refreshing = false){
+    const newPage = refreshing ? 1 : this.page + 1;
+    this.setState({ refreshing });
+    fetch(`https://api.github.com/search/repositories?q=react&page=${newPage}`)
+      .then(response => response.json())
+      .then(({ items }) => {
+        this.page = newPage;
+        if (refreshing) {
+          this.setState({ items });
+        } else {
+          this.setState({ items: [...this.state.items, ...items] });
+        }
+    });        
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={{marginTop: 20}} onPress={() => this.fetchRepositories()}>
+          <Text>Fetch</Text>
+        </TouchableOpacity>
+        <FlatList
+          data={this.state.items}
+          renderItem={({ item }) => <Text style={{padding: 20}}>{item.name}</Text>}
+          keyExtractor={(item) => item.id}
+          onEndReached={() => this.fetchRepositories(true)}
+          onEndReachedThreshold={0.1}
+          onRefresh={() =>{}}
+          refreshing={this.state.refreshing}
+          />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+});
+/[^\d]/
