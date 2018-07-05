feedc02bc7c43d543f2d707397cf8ac3f5b4dcd4
山本　憲 <em072029@gmail.com>
2018-07-06 08:26:10 +0900

    display button

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, Text, TextInput, View, Button } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
-export default class PizzaTranslator extends Component {
-
+export default class ButtonBasics extends Component {
+  _onPressButton(){
+    Alert.alert('You tapped the button!')
+  }
   render(){
-    return (
-      <View style={{padding: 10, backgroundColor: 'gray', margin: 100}}>
-        <Button
-          onPress={() => {
-            Alert.alert('you tapped the button!');
-          }}
-          title="press me"
-        />
+    return(
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="press me"
+            color="#841584"
+            />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer} >
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="OK!"
+            color="#841584"
+            />
+        </View>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
 
-AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
+
+AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
