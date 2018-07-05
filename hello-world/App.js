b52d618c74de3821292447b2ee5b8d03da3eccf4
山本　憲 <em072029@gmail.com>
2018-07-06 03:31:46 +0900

    handle text input

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, Text, TextInput, View } from 'react-native';
 
-export default class FlexDimensionsBasics extends Component {
+export default class PizzaTranslator extends Component {
+  constructor(props){
+    super(props);
+    this.state = {text: ''};
+  }
+
   render(){
     return (
-      // <View style={{flex: 2, flexDirection:'row'}}>
-      <View style={{flex: 2, flexDirection:'column'}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      <View style={{padding: 10, backgroundColor: 'gray', margin: 100}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({ text })}
+          />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text }
+        </Text>
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);
+
+AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
