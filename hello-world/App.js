59bfaed0852755c6fb88a046afe417ec74f7ddcf
山本　憲 <em072029@gmail.com>
2018-07-06 01:34:01 +0900

    flexbox 勉強

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
+export default class FlexDimensionsBasics extends Component {
+  render(){
     return (
-      <Text>{display}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
-  render() {
-    return (
-      <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
+      // <View style={{flex: 2, flexDirection:'row'}}>
+      <View style={{flex: 2, flexDirection:'column'}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
 }
 
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
+AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);
