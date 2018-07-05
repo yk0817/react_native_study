5221a1dbff2dee2e46e3859219425ba0c5aaf64a
山本　憲 <em072029@gmail.com>
2018-07-06 00:55:28 +0900

    Srate 写経

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  state = {
-    message: 'Message',
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
   }
 
   render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
     return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-        <HelloWorld message={this.state.message} />
-        <Text
-          onPress={() => this.setState({ message: 'Press' })}
-        >
-        {this.state.message}
-        </Text>
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
 }
 
-class HelloWorld extends React.Component {
-  render(){
-    return (<Text>Hello World {this.props.message}</Text>);
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
