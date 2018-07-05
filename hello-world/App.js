7ceee4202fbc8ef6f3201624c4e58123f1587e4e
山本　憲 <em072029@gmail.com>
2018-07-06 03:42:51 +0900

    tapped!

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
+import { Alert, AppRegistry, Text, TextInput, View, Button } from 'react-native';
 
 export default class PizzaTranslator extends Component {
-  constructor(props){
-    super(props);
-    this.state = {text: ''};
-  }
 
   render(){
     return (
       <View style={{padding: 10, backgroundColor: 'gray', margin: 100}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({ text })}
-          />
-        <Text style={{ padding: 10, fontSize: 42 }}>
-          {this.state.text }
-        </Text>
+        <Button
+          onPress={() => {
+            Alert.alert('you tapped the button!');
+          }}
+          title="press me"
+        />
       </View>
     );
   }
