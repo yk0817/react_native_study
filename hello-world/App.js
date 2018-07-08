29c92ed14aeb5453252805520ebc0c1a5294e2c0
山本　憲 <em072029@gmail.com>
2018-07-08 14:48:12 +0900

    prop state解説用

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,42 +1,68 @@
 import React, { Component } from 'react';
-import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
 
-export default class FetchExample extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = { isLoading: true}
-  }
-  compnentDidMount(){
-    return fetch('https://facebook.github.io/react-native/movies.json')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson.movies,
-        }, function(){
-
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+export default class Test extends React.Component {
   render(){
-    if (this.state.isLoading) {
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
-    return(
-      <View style={{flex: 1, paddingTop: 20}}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
-          keyExtractor={(item, index) => index}
-          />
+    return (
+      <View style={{alignItems: 'center', marginTop: 200}}>
+       <Text>Hello World</Text>
       </View>
     );
   }
 }
+
+// class Hello extends React.Component {
+//   render(){
+//     return (
+//       <Text>Hello {this.props.name}!</Text>
+//     );
+//   }
+// }
+//
+// export default class LotsOfGreentings extends React.Component {
+//   render(){
+//     return (
+//       <View style={{alignItems: 'center', marginTop: 200}}>
+//         <Hello name='props1' />
+//         <Hello name='props2' />
+//         <Hello name='props3' />
+//       </View>
+//     );
+//   }
+// }
+
+// class Blink extends React.Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {isShowingText: true};
+  //
+  //   setInterval(() => {
+  //     // console.log(this.state);
+  //     this.setState(previousState => {
+  //     console.log(previousState);
+  //     return { isShowingText: !previousState.isShowingText };
+  //     });
+  //   }, 1000);
+  // }
+  // render(){
+    // let display = this.state.isShowingText ? this.props.text : ' ';
+    // return (
+    //   <Text>{display}</Text>
+    // );
+//   }
+// }
+
+
+
+// export default class BlinkApp extends React.Component {
+//   render(){
+//     return (
+//       <View style={{alignItems: 'center', marginTop: 200}}>
+//         <Blink text='state1' />
+//         <Blink text='state2' />
+//         <Blink text='state3' />
+//         <Blink text='state4' />
+//       </View>
+//     );
+//   }
+// }
