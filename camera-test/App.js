84d1f3ded68119226cb525a6f77ae36059c55056
山本　憲 <em072029@gmail.com>
2018-07-22 10:35:16 +0900

    カメラテスト　パーミッションまだ

---
+++ camera-test/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ImagePickerSample from './src/components/ImagePicker';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <ImagePickerSample />
+    );
+  }
+}
